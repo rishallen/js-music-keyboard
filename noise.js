90d4c28964719eff4704519acf8f104bdec3e29d
Risha <rishallen@gmail.com>
2016-06-29 15:54:57 -0700

    created javascript functionality for a music keyboard.

--- noise.js
+++ noise.js
@@ -1,3 +1,35 @@
-$(document).ready( function() {
   // your code here
+$(document).ready( function() {
+  // console.log("this is a test");
+
+  var container = $('.instrument');
+  // console.log(container);
+  var buttons = container.children('button.note');
+
+    buttons.on('click', function(event) {
+    event.preventDefault();
+
+    function playNote(id) {
+      document.getElementById(id).load();
+      document.getElementById(id).play();
+    }
+
+    var button = $(this);
+    if (button.hasClass('c')) {
+      playNote("cAudio");
+      // console.log('c');
+    } else if (button.hasClass('d')){
+        playNote("dAudio");
+    } else if (button.hasClass('e')){
+        playNote("eAudio");
+    } else if (button.hasClass('f')){
+        playNote("fAudio");
+    } else if (button.hasClass('g')){
+        playNote("gAudio");
+    } else if (button.hasClass('a')){
+        playNote("aAudio");
+    } else if (button.hasClass('b')){
+        playNote("bAudio");
+    }
+  });
 });
